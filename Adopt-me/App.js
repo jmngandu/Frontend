9f42be50a31d34f19428205212f81ca75abc5851
James Ngandu <jamesmngandu@gmail.com>
2023-08-16 23:20:42 +0300

    added

--- Adopt-me/App.js
+++ Adopt-me/App.js
@@ -1,11 +1,1 @@
-const App = () => {
-    return React.createElement(
-      "div",
-      {},
-      React.createElement("h1", {}, "Adopt Me!")
-    );
-  };
-  
-  const container = document.getElementById("root");
-  const root = ReactDOM.createRoot(container);
-  root.render(React.createElement(App));
+
