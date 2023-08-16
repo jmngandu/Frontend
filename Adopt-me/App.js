a1b04302add4e785f3414cece7cfc59143576d5f
James Ngandu <jamesmngandu@gmail.com>
2023-08-16 23:37:09 +0300

    added

--- Adopt-me/App.js
+++ Adopt-me/App.js
@@ -1,10 +1,18 @@
+const pet = () =>{
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "codie"),
+    React.createElement("h2", {}, "12 years"),
+    React.createElement("h3", {}, "German shepherd")
+  ])
+};
 const App = () => {
   return React.createElement(
     "div", 
-    {}, 
-    React.createElement("h1", {}, "i love you")
+    {},[
+    React.createElement("h1", {}, "i love you"),
+    React.createElement(pet)]
   );
-};
+}; 
 
 const root = document.getElementById("root");
 const container = ReactDOM.createRoot(root);
