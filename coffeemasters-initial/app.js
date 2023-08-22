fe198a4674fbb7f1ef9fccae2e52805b7397eac9
James Ngandu <jamesmngandu@gmail.com>
2023-08-22 23:30:40 +0300

    added

--- coffeemasters-initial/app.js
+++ coffeemasters-initial/app.js
@@ -1,9 +1,4 @@
 
 window.addEventListener("DOMContentLoaded", () =>{
-    let nav = document.querySelector("nav");
-    nav.innerHTML = `
-    <h2> Hello World </h2>
-    <p> I am james ngandu </p>`
-   
     
 });
