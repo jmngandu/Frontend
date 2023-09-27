073283f3e027fbfa7c8f4646c3e71b28440448b1
James Ngandu <jamesmngandu@gmail.com>
2023-09-28 01:46:49 +0300

    added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,12 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      JTHREEGLOBALLY
       </div>
       <h1>Community Site</h1>
       <div className="card">
