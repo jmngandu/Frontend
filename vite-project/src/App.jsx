ffc8d984f5f88844f68c44d3b1dd11380cdca0f7
James Ngandu <jamesmngandu@gmail.com>
2023-09-28 01:39:14 +0300

    added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Community Site</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Good times and great hits
         </p>
       </div>
       <p className="read-the-docs">
