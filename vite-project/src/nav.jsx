f91d859c604ab0bcc3c4c84381f787dacaffafb7
James Ngandu <jamesmngandu@gmail.com>
2023-09-28 00:57:26 +0300

    added

--- vite-project/src/nav.jsx
+++ vite-project/src/nav.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import React from "react"
 
 export default function navBar(){
     return(
-     <div>
-    <   h1>JTHREEGLOBALLY</h1>
+     <>
+        <h1>JTHREEGLOBALLY</h1>
       <ul>
         <li>HOME</li>
         <li>SERVICES</li>
         <li>ABOUT</li>
         <li>CONTACTS</li>
       </ul>
-     </div>
+     </>
   )
   }
