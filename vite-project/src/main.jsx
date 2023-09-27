2ad310dedbfe238cb080eedb54bff64c18bbea28
James Ngandu <jamesmngandu@gmail.com>
2023-09-28 01:13:20 +0300

    added

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import navBar from './navBar.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
