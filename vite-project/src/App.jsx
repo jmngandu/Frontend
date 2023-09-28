c2332fc87dde3a781994cc4adf431cad751cab6c
James Ngandu <jamesmngandu@gmail.com>
2023-09-28 07:29:27 +0300

    added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar />
       <div>
       JTHREEGLOBALLY
       </div>
---
+++ vite-project/src/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./NavBar.css"
+
+export default function NavBar(){
+    return(
+      <div className="the-bar">
+          <l1>JTHREEGLOBALLY</l1>
+          <ul className="nav-ul">
+              <li>HOME</li>
+              <li>SERVICES</li>
+              <li>ABOUT</li>
+              <li>CONTACTS</li>
+          </ul>
+          </div>
+    )
+}
