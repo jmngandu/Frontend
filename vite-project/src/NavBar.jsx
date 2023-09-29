10eec7fd53d8f3418866018a88e1284684e8f9f2
James Ngandu <jamesmngandu@gmail.com>
2023-09-29 16:44:01 +0300

    added

--- vite-project/src/NavBar.jsx
+++ vite-project/src/NavBar.jsx
@@ -5,12 +5,11 @@ export default function NavBar(){
     return(
            
       <div className="the-bar">
-      <l1>JTHREEGLOBALLY</l1>
-          <ul className="nav-ul">
+       <ul className="nav-ul">
               <li>HOME</li>
               <li>SERVICES</li>
               <li>ABOUT</li>
-              <li>CONTACTS</li>
+              <li className="lid">CONTACTS</li>
           </ul>
           </div>
            )
