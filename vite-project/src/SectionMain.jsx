1188bc71b96d607bc8fedb0fb7954c616edd5dc8
James Ngandu <jamesmngandu@gmail.com>
2023-10-01 00:04:17 +0300

    commit

--- vite-project/src/SectionMain.jsx
+++ vite-project/src/SectionMain.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { preprocessCSS } from "vite";
 import "./SectionMain.css"
 
-export default function SectionMain(props) {
+export default function SectionMain(fsevents) {
     return(
         <div className="wrapper">
-            <img src={props.img} className="cars"/>
+            <div className="cars">
+            <img src={props.img} />
+            </div>
             <h3 className="brand">{props.brand}</h3>
             <h4 className="type">{props.type}</h4>
             <h5 className="cost">{props.cost}</h5>
