532b1fd1c09a0d831c0ef5938ddff3fce73a43bc
James Ngandu <jamesmngandu@gmail.com>
2023-09-30 22:05:15 +0300

    added

--- vite-project/src/SectionMain.jsx
+++ vite-project/src/SectionMain.jsx
@@ -31,17 +31,7 @@ export default function SectionMain() {
             </div>
         </div>
         
-        <div className="contact-card">
-            <img src="./images/felix.png"/>
-            <h3>Felix</h3>
-            <div className="info-group">
-                <img src="./images/phone-icon.png" />
-                <p>(212) 555-4567</p>
-            </div>
-            <div className="info-group">
-                <img src="./images/mail-icon.png" />
-                <p>thecat@hotmail.com</p>
-            </div>
+        <div class
         </div>
         
         </div>
