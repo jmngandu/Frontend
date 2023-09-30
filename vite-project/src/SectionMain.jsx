a8e462a34c0ea5dc92cf1e625fbf4b316095380e
James Ngandu <jamesmngandu@gmail.com>
2023-09-30 22:06:20 +0300

    added

--- vite-project/src/SectionMain.jsx
+++ vite-project/src/SectionMain.jsx
@@ -3,31 +3,6 @@ import "./SectionMain.css"
 
 export default function SectionMain() {
     return(
-        <div className="contacts">
-        
-        <div className="contact-card">
-            <img src="../assets/muk.jpg"/>
-            <h3>Mr. Whiskerson</h3>
-            <div className="info-group">
-                <img src="./src/assets/muk.jpg" />
-                <p>(212) 555-1234</p>
-            </div>
-            <div className="info-group">
-                <img src="./images/mail-icon.png" />
-                <p>mr.whiskaz@catnap.meow</p>
-            </div>
-        </div>
-        
-        <div className="contact-card">
-            <img src="./images/fluffykins.png"/>
-            <h3>Fluffykins</h3>
-            <div className="info-group">
-                <img src="./images/phone-icon.png" />
-                <p>(212) 555-2345</p>
-            </div>
-            <div className="info-group">
-                <img src="./images/mail-icon.png" />
-                <p>fluff@me.com</p>
-            <
+        <h1>JTHREEGLOBALLY</h1>
     )
 }
