98b252371a7c2137a5d84f068f0a3b2e8a064c2b
James Ngandu <jamesmngandu@gmail.com>
2023-10-01 19:06:25 +0300

    commit

--- vite-project/src/NavBar.jsx
+++ vite-project/src/NavBar.jsx
@@ -5,7 +5,6 @@ export default function NavBar(){
     return(
            
       < nav className="the-bar">
-        
         <h1>JM-NGANDU</h1>
                <ul className="nav-ul">
               <li className="yes1">HOME</li>
